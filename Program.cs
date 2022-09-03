edd3117941ef7db5d547d9b363720e2cac1a189a
MisterGrad <mistergrad@icloud.com>
2022-09-03 13:48:47 +0500

    123

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+// int count = 0;
+
+// while (count <= 100)
+// {
+//     count = count + 1;
+//     int posX = new Random().Next (0, 30), posY = new Random().Next (0, 30);
+
+// Console.SetCursorPosition(posX,posY);
+// Console.Write("+");
+// }
+
+int xa = 60, ya = 1,
+    xb = 1, yb = 40,
+    xc = 120, yc = 40;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0,3);
+    if(what == 0)
+     {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+      if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+  if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+
+}
